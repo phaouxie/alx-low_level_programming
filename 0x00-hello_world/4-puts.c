e167320c386214ffef1cff7455d56572cf855fff
phaouxie <fauziyya.hayatu@aun.edu.ng>
2022-06-30 15:14:09 -0700

    lolo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"programming is like building multilingual puzzle");
-	return (0);
+{puts("\"Programming is like building multilingual puzzle");
+return (0);
 }	
 
