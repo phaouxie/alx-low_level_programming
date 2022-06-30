23bd442397b65be0e7318be1c56b48d7d17a2032
phaouxie <fauziyya.hayatu@aun.edu.ng>
2022-06-30 16:41:52 -0700

    kill

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.>
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 0 (Success)
- * /
- int main(void)
- {
- 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0)
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
