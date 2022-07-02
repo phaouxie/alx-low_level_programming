ee7c87c69f8ab740fb5ec9664d298a4c12123c8d
phaouxie <fauziyya.hayatu@aun.edu.ng>
2022-07-02 14:36:02 -0700

    kiki

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include<time.h>
 #include<stdio.h>
 /**
-* main -> assign a random number to the variable n each time it is executed and print
+* main -> assign a random number
 * based a condition
 * Return : always 0
 */
