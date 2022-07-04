d27749067a88f91cd881bbf18cccaa24bc74e730
phaouxie <fauziyya.hayatu@aun.edu.ng>
2022-07-04 10:16:37 -0700

    juju

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,19 +14,19 @@ int a;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-a = n % 10;
-printf("Last digit of %d is ", n,a);
+a = n%10;
+printf("Last digit of %d is %d ", n,a);
 if (a > 5)
 {
-printf("greater than 5");
+printf("and is greater than 5");
 }
 if (a == 0)
 {
-	printf("0");
+	printf("and is 0");
 }
 if (a < 6 && a != 0)
 {
-printf("less than 6 not 0");
+printf("and is less than 6 not 0");
 }
 printf("\n");
 return (0);
