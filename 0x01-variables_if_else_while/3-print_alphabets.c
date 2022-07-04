d73e10e3d3c408999beb6d6cb1923bd0222350b5
phaouxie <fauziyya.hayatu@aun.edu.ng>
2022-07-04 11:46:01 -0700

    juju

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ *prints alphabets in lowercase and uppercase
+ *
+ *main- Entry point
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+char ch;
+char ab;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ab = 'A'; ab <= 'Z'; ab++)
+{
+putchar(ab);
+}
+putchar('\n');
+return (0);
+}
+
