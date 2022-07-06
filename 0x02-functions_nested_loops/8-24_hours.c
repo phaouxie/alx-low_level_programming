867ec190eadd25dea3d2932b9d7b8b73f85bcd6f
phaouxie <fauziyya.hayatu@aun.edu.ng>
2022-07-06 15:35:11 -0700

    tired

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ for (j = 0; j < 60; j++)
 if (i < 10)
 {
 _putchar('0');
-_putchar(i + '0')
+_putchar(i + '0');
 }
 else if (i >= 10)
 {
@@ -30,6 +30,6 @@ _putchar(':');
 _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
 }
-_putcha('\n');
+_putchar('\n');
 }
 }
