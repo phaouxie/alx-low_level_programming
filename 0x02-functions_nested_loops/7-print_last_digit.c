7294e7b175916b92a72d36c05c4be264c5c20832
phaouxie <fauziyya.hayatu@aun.edu.ng>
2022-07-06 14:40:21 -0700

    digitd

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,15 @@
 int print_last_digit(int n)
 {
 int x;
-if (n > 0)
-n = -n;
-x = n % 10;
+if (n < 0)
+{
+x = (-1 * (n % 10));
 _putchar(x + '0');
 return (x);
+}
+else
+{
+x = (n % 10);
+return (x);
+}
 }
